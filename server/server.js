266d43abcab24a56531812fd48ca207ae82d421d
Robby Newman <robbynewman@Robbys-MacBook-Pro.local>
2016-02-18 12:51:29 -0600

    a whole mess of stuff

--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ app.use(express.static('server/public'));
 app.use('/', index);
 app.use('/api', api);
 
+app.use(express.static('server/public'));
+
 var server = app.listen(process.env.PORT || 3000, function(){
     var port = server.address().port;
     console.log("Listening on port", port);
