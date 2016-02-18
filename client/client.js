29756d446560a711394b42950b535b3124609146
Robby Newman <robbynewman@Robbys-MacBook-Pro.local>
2016-02-18 09:53:35 -0600

    connect ng-app and main controller

--- client/client.js
+++ client/client.js
@@ -1,3 +1,13 @@
 /**
  * Created by jeremycloutier on 2/16/16.
  */
+
+var app = angular.module('myApp', []);
+
+app.controller('MainController', [ '$scope', 'SomeService', function($scope, SomeService){
+
+}]);
+
+app.factory('SomeService', ['$http', function($http){
+
+}])
