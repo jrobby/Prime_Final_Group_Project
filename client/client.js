d1bf3d86781aeae4933c7d390f5e0f70cacc0b48
Matthew Reinders <mcareinders@yahoo.com>
2016-02-22 16:52:05 -0600

    started functionality to count # served.  Dates don't quite work out right yet

--- client/client.js
+++ client/client.js
@@ -3,7 +3,6 @@
  */
 
 var app = angular.module('myApp', ['ngRoute']);
-console.log('client.js is connected to index.html');
 
 app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
     $routeProvider
@@ -23,6 +22,34 @@ app.config(['$routeProvider', '$locationProvider', function($routeProvider, $loc
 app.controller('MainController', [ '$scope', '$location', 'SmartSheetService', function($scope, $location, SmartSheetService){
 
     $scope.endDate = new Date();
+    //sets default start date to 05-07-2012
+    $scope.startDate = new Date('2012-05-08');
+
+    //display the dates selected
+    $scope.submitDate = function(){
+      console.log('start date selected', $scope.startDate);
+      console.log('end date selected', $scope.endDate);
+
+      //Test function to do the call to get all the Smartsheet data
+      //returns an array of objects with the columns we need
+        SmartSheetService.getSmartSheetData().then(function(response){
+          $scope.smartSheetData = response.data;
+          console.log($scope.smartSheetData);
+
+          //count the number served over the selected date range
+          //the count doesn't quite work because the new Date() reads the date as one day
+          //earlier than it actually is on the spreadsheet
+          $scope.numServed = 0;
+          for(var i=0; i<response.data.length; i++){
+            var classStart = new Date(response.data[i].classStart);
+            console.log(classStart);
+            if(classStart >= $scope.startDate && classStart <= $scope.endDate){
+              $scope.numServed++;
+            }
+          }
+          console.log('number served', $scope.numServed);
+        });
+    };
 
     $scope.demographicList = ['Age', 'Gender', 'Race', 'Veteran Status']; // More here, possibly?
     $scope.progressList = ['3mo', '6mo', '1yr', '2yr', '3yr', '4yr', '5yr'];
@@ -41,16 +68,6 @@ app.controller('MainController', [ '$scope', '$location', 'SmartSheetService', f
     $scope.hideAverageSalary = function (){
         $scope.averageShow = false;
     };
-
-    //Test function to do the call to get all the Smartsheet data
-    //returns an array of objects with the columns we need
-    $scope.getSmartSheetData = function(){
-      SmartSheetService.getSmartSheetData().then(function(response){
-        $scope.smartSheetData = response.data;
-        console.log($scope.smartSheetData);
-      });
-    };
-
 
 }]);
 
