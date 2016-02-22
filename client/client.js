edc8eaa9238f472b9f2b5589836884b0ee714f57
JeremyCloutier <clout066@umn.edu>
2016-02-22 11:15:57 -0600

    Changes dropdown names to free up variable names.

--- client/client.js
+++ client/client.js
@@ -24,8 +24,8 @@ app.controller('MainController', [ '$scope', '$location', function($scope, $loca
 
     $scope.endDate = new Date();
 
-    $scope.demographics = ['Age', 'Gender', 'Race', 'Veteran Status']; // More here, possibly?
-    $scope.progresses = ['3mo', '6mo', '1yr', '2yr', '3yr', '4yr', '5yr'];
+    $scope.demographicList = ['Age', 'Gender', 'Race', 'Veteran Status']; // More here, possibly?
+    $scope.progressList = ['3mo', '6mo', '1yr', '2yr', '3yr', '4yr', '5yr'];
 
     $scope.tab = 'a';
     $scope.averageShow = false;
